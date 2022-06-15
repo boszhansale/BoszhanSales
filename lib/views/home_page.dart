47ce6a9b8eb43a562a64c8b31327d9bd108113db
Thanos <iamthegreatthanos@gmail.com>
2022-06-15 13:10:28 +0600

    Added blocker.

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -559,7 +559,7 @@ class _HomePageState extends State<HomePage> {
     var result = await AuthProvider().checkApplicationVersion();
 
     if (result != 'Error') {
-      if (result['version'] != '0.6') {
+      if (result['version'] != '0.7') {
         setState(() {
           newVersion = true;
         });
